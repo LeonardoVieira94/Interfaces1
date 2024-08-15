1a42d72e012ca2b287ff0eabd04244cca111a2b7
Leonardo Vieira <leonardovieira_94@hotmail.com>
2024-08-15 17:51:51 -0300

    Exercicio resolvido interfaces

---
+++ Interfaces1/Program.cs
@@ -0,0 +1,37 @@
+﻿using Interfaces1.Entities;
+using Interfaces1.Services;
+using System.Globalization;
+
+namespace Interfaces1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data:");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/mm/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/mm/yyyy hh:mm): ");
+            DateTime end = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, end, new Vehicle(model));
+
+            Console.Write("Enter price per hour: ");
+            double pricePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double pricePerDay = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("Invoice");
+            Console.WriteLine(carRental.Invoice);
+
+
+
+        }
+    }
+}
